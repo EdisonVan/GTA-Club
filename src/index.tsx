c45f2fc2d0f4f43e90ab2bb3fa49d06c8224da2f
PengSifan <vanjohnson@foxmail.com>
2020-02-06 19:49:00 +0800

    modify package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 // import Hello from './components/demo/Hello';
-import * as serviceWorker from './serviceWorker';
-
+import './index.css';
+import Root from './routers';
 ReactDOM.render(
     // <Hello name="TypeScript" />,
-    <App />, document.getElementById('root'));
+    <Root />,
+    document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from '../components/App';
 
-const Root = () => {
+const Root = () => (
     <BrowserRouter>
-        <Route path="/*" component={App} />
+        <Route path="/*" component={App}/> 
+        {/* 将整个应用定义为一个路由，再通过BrowserRouter包装一下，返回到一个Root */}
     </BrowserRouter>
-}
+)
+
 export default Root;
