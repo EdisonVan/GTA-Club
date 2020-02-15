8fbf6b75d04f3953830de3215c94e2981b068df3
EdisonVan <vanjohnson@foxmail.com>
2020-02-16 07:40:41 +0800

    axios

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -19,3 +19,20 @@ axios.interceptors.response.use(
     return Promise.reject(error);
   }
 );
+// 封装get
+export function get(url: string, data: any) {
+    return axios.get(url, {
+        params: data
+    });
+};
+
+// 自定义封装post，将其发送数据改造为表单格式，后端就不用做json解析了
+export function post(url: string, data: any) {
+    return axios({
+        method: 'post',
+        url,
+        data
+    });
+};
+
+export default axios;
