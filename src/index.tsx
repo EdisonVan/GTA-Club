21dfd01db37c577dac799f7bc100b3f42194eb71
EdisonVan <vanjohnson@foxmail.com>
2020-02-16 08:41:20 +0800

    icon replace

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import Hello from './components/demo/Hello';
-import './index.css';
-import Root from './routers';
+import "./index.css";
+import Root from "./routers";
 ReactDOM.render(
-    // <Hello name="TypeScript" />,
-    <Root />,
-    document.getElementById('root')
+  // <Hello name="TypeScript" />,
+  <Root />,
+  document.getElementById("root")
 );
-
-
