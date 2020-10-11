179ee89202299e8a32b50c291600c312e9c6ccec
EdisonVan <vanjohnson@foxmail.com>
2020-10-11 22:34:53 +0800

    GTA-Club

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import { Layout, ConfigProvider } from "antd";
 import zhCN from "antd/es/locale/zh_CN";
+import Footer from "./Footer";
+import AddTodo from "../containers/AddTodo";
+import VisibleTodoList from "../containers/VisibleTodoList";
 // import logo from "./logo.svg";
 import Index from "./index";
 import "./App.less";
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content } = Layout;
+// , Footer
 
 const App = ({ match }: any) => {
   console.log(match.url);
@@ -14,6 +18,10 @@ const App = ({ match }: any) => {
   // match.url当作当前hash，去掉hash之前的/,如果为空，默认指定为home
   return (
     <ConfigProvider locale={zhCN}>
+      <div>
+        <AddTodo />
+        <VisibleTodoList />
+      </div>
       <Layout>
         <Header>
           {/* 公共头部   */}
@@ -24,7 +32,9 @@ const App = ({ match }: any) => {
             <Route path="/" exact component={Index} />
           </div>
         </Content>
-        <Footer className="footer">GTA Club@2020</Footer>
+        {/* <Footer className="footer"> */}
+        <Footer />
+        {/* </Footer> */}
       </Layout>
     </ConfigProvider>
   );
